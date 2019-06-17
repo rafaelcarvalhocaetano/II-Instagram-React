c8bba457e3fd060cb8c8dbee60a56261205281be
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-06-17 19:16:26 -0300

    finsi

--- src/index.js
+++ src/index.js
@@ -2,9 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './global.css';
-const cors = require('./cors');
-const express = require('express');
-const app = express();
-app.use(cors);
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,5 @@
 import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://backend-instagram.herokuapp.com/',
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json',
-    'X-Custom-Header': 'foobar',
-    'X-Requested-With': 'XMLHttpRequest',
-    'content-type': 'application/x-www-form-urlencoded',
-    'mode': 'no-cors',
-  }
-});
-
-api.defaults.baseURL = 'https://backend-instagram.herokuapp.com';
-api.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-
+  const api = axios.create({
+    baseURL: 'https://backend-instagram.herokuapp.com',
+  });
 export default api;
