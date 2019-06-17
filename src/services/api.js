b4cadcc3a1693be50bb6a55ad9a537b36838f4b0
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-06-17 15:57:47 -0300

    New Updating

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backend-instagram.herokuapp.com/'
+  baseURL: 'https://backend-instagram.herokuapp.com/',
+  timeout: 1000,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+    'Authorization': true,
+    withCredentials: true,
+    mode: 'no-cors',
+  }
 });
 
 export default api;
